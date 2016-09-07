68ebcca7fb83329dd214ba1dbd0cee1787e54e42
Joshua Buker <buker.joshua@gmail.com>
2016-09-07 01:16:46 -0700

    Add foolproofing to autorun_tour.bat

--- autorun_tour.bat
+++ autorun_tour.bat
@@ -8,6 +8,7 @@ echo.
 set current=8
 set /p final=Enter Final Board Size)
 echo.
+if %current% gtr %final% goto toosmall
 title Knight's Tour Calculator - Running
 :begin
 color 0F
@@ -19,9 +20,16 @@ set /a sX=%current%/2
 set /a sY=%current%/2
 "%cd%\knights_tour.exe" %sX% %sY% %bX% %bY%
 echo.
+if %current% gtr %final% goto eof
 if %current%==%final% goto eof
 set /a current=%current%*2
 goto begin
+:toosmall
+title Knight's Tour Calculator - Input Too Small
+echo %final% is too small, please use a value greater than %current%
+echo.
+pause
+exit
 :eof
 title Knight's Tour Calculator - Finished
 pause
